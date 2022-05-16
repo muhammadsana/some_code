4b55ba4061dc08dc4be50146cf8181a533f1816d
muhammadsana <104550483+muhammadsana@users.noreply.github.com>
2022-05-16 13:02:56 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,7 +1,7 @@
-Sudo apt –y update
+sudo apt-get –y update
 Java –version
-Sudo apt –get install –y git maven
+sudo apt –get install –y git maven
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \ /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \ /etc/apt/sources.list.d/jenkins.list > /dev/null 
-Sudo apt-get update
-Sudo apt-get install
+sudo apt-get update
+sudo apt-get install
